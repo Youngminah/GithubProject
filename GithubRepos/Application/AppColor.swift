83ea38820a256459452d87a587e22e71da5206f3
Youngminah <bluebelsmudi@gmail.com>
2022-03-12 16:47:35 +0900

    feat: 커스텀 컬러

---
+++ GithubRepos/Application/AppColor.swift
@@ -0,0 +1,23 @@
+//
+//  AppColor.swift
+//  GithubRepos
+//
+//  Created by meng on 2022/03/12.
+//
+
+import Foundation
+
+enum AppColor: String {
+
+    case coral400
+    case coral500
+
+    case gray100
+    case gray200
+    case gray300
+    case gray400
+    case gray500
+    case gray600
+
+    case yellow200
+}
---
+++ GithubRepos/Presentation/Extension/UIColor+Ex.swift
@@ -0,0 +1,39 @@
+//
+//  UIColor+Ex.swift
+//  GithubRepos
+//
+//  Created by meng on 2022/03/12.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+
+    static var coral400: UIColor {
+        return UIColor(named: AppColor.coral400.rawValue)!
+    }
+    static var coral500: UIColor {
+        return UIColor(named: AppColor.coral500.rawValue)!
+    }
+    static var gray100: UIColor {
+        return UIColor(named: AppColor.gray100.rawValue)!
+    }
+    static var gray200: UIColor {
+        return UIColor(named: AppColor.gray200.rawValue)!
+    }
+    static var gray300: UIColor {
+        return UIColor(named: AppColor.gray300.rawValue)!
+    }
+    static var gray400: UIColor {
+        return UIColor(named: AppColor.gray400.rawValue)!
+    }
+    static var gray500: UIColor {
+        return UIColor(named: AppColor.gray500.rawValue)!
+    }
+    static var gray600: UIColor {
+        return UIColor(named: AppColor.gray600.rawValue)!
+    }
+    static var yellow200: UIColor {
+        return UIColor(named: AppColor.yellow200.rawValue)!
+    }
+}
