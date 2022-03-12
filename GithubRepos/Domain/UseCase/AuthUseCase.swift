76e356640f1550c73fcbd1c49b29c447f15dce38
Youngminah <bluebelsmudi@gmail.com>
2022-03-13 01:19:09 +0900

    add: auth 유즈케이스

---
+++ GithubRepos/Domain/UseCase/AuthUseCase.swift
@@ -0,0 +1,16 @@
+//
+//  AuthUseCase.swift
+//  GithubRepos
+//
+//  Created by meng on 2022/03/13.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+final class AuthUseCase {
+
+    private let disposeBag = DisposeBag()
+
+}
