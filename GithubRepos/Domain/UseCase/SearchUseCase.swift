d5d9752985cf0b57637ac90a8ee5453d12f348c7
Youngminah <bluebelsmudi@gmail.com>
2022-03-13 01:16:44 +0900

    feat: 검색 유즈케이스

---
+++ GithubRepos/Domain/UseCase/SearchUseCase.swift
@@ -0,0 +1,67 @@
+//
+//  SearchUseCase.swift
+//  GithubRepos
+//
+//  Created by meng on 2022/03/12.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+final class SearchUseCase {
+
+    private let githubRepository: GithubRepositoryType
+    private let disposeBag = DisposeBag()
+
+    let successReqeustSearch = PublishRelay<Repos>()
+    let successReqeustStar = PublishRelay<Void>()
+    let successReqeustUnstar = PublishRelay<Void>()
+    let failGithubError = PublishRelay<GithubServerError>()
+
+    init(
+        githubRepository: GithubRepositoryType
+    ){
+        self.githubRepository = githubRepository
+    }
+}
+
+extension SearchUseCase {
+
+    func requestSearch(searchName: String, page: Int) {
+        let query = ReposQuery(searchName: searchName)
+        self.githubRepository.requestSearch(query: query, page: page) { [weak self] response in
+            guard let self = self else { return }
+            switch response {
+            case .success(let repos):
+                self.successReqeustSearch.accept(repos)
+            case .failure(let error):
+                self.failGithubError.accept(error)
+            }
+        }
+    }
+
+    func requestStar(owners: String, repos: String) {
+        self.githubRepository.requestStar(owners: owners, repos: repos) { [weak self] response in
+            guard let self = self else { return }
+            switch response {
+            case .success(_):
+                self.successReqeustStar.accept(())
+            case .failure(let error):
+                self.failGithubError.accept(error)
+            }
+        }
+    }
+
+    func requestUnStar(owners: String, repos: String) {
+        self.githubRepository.requestUnstar(owners: owners, repos: repos) { [weak self] response in
+            guard let self = self else { return }
+            switch response {
+            case .success(_):
+                self.successReqeustUnstar.accept(())
+            case .failure(let error):
+                self.failGithubError.accept(error)
+            }
+        }
+    }
+}
