6caabcebb7332dce34c48d4558a155dc8592dda7
Youngminah <bluebelsmudi@gmail.com>
2022-03-13 19:17:42 +0900

    feat: 언어 케이스화

---
+++ GithubRepos/Utils/Case/LanguageCase.swift
@@ -0,0 +1,35 @@
+//
+//  LanguageCase.swift
+//  GithubRepos
+//
+//  Created by meng on 2022/03/13.
+//
+
+import UIKit
+
+enum LanguageCase: String, CaseIterable {
+
+    case swift = "Swift"
+    case ruby = "Ruby"
+    case css = "CSS"
+    case c = "C"
+    case python = "Python"
+    case unknown
+
+    var color: UIColor {
+        switch self {
+        case .swift:
+            return .orange
+        case .ruby:
+            return .red
+        case .css:
+            return .green
+        case .c:
+            return .yellow200
+        case .python:
+            return .purple
+        case .unknown:
+            return .gray500
+        }
+    }
+}
