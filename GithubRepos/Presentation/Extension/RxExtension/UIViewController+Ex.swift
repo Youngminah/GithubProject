912e94caae5e0634e0de39ace0f7cacd89b0d8fc
Youngminah <bluebelsmudi@gmail.com>
2022-03-13 19:18:41 +0900

    feat: vc 생명주기 rx 익스텐션

---
+++ GithubRepos/Presentation/Extension/RxExtension/UIViewController+Ex.swift
@@ -0,0 +1,70 @@
+//
+//  UIViewController+Ex.swift
+//  GithubRepos
+//
+//  Created by meng on 2022/03/13.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+extension Reactive where Base: UIViewController {
+
+    var viewDidLoad: ControlEvent<Void> {
+        let source = self.methodInvoked(#selector(Base.viewDidLoad)).map { _ in }
+        return ControlEvent(events: source)
+    }
+
+    var viewWillAppear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(Base.viewWillAppear)).map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
+    }
+    var viewDidAppear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(Base.viewDidAppear)).map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
+    }
+
+    var viewWillDisappear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(Base.viewWillDisappear)).map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
+    }
+    var viewDidDisappear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(Base.viewDidDisappear)).map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
+    }
+
+    var viewWillLayoutSubviews: ControlEvent<Void> {
+        let source = self.methodInvoked(#selector(Base.viewWillLayoutSubviews)).map { _ in }
+        return ControlEvent(events: source)
+    }
+    var viewDidLayoutSubviews: ControlEvent<Void> {
+        let source = self.methodInvoked(#selector(Base.viewDidLayoutSubviews)).map { _ in }
+        return ControlEvent(events: source)
+    }
+
+    var willMoveToParentViewController: ControlEvent<UIViewController?> {
+        let source = self.methodInvoked(#selector(Base.willMove)).map { $0.first as? UIViewController }
+        return ControlEvent(events: source)
+    }
+    var didMoveToParentViewController: ControlEvent<UIViewController?> {
+        let source = self.methodInvoked(#selector(Base.didMove)).map { $0.first as? UIViewController }
+        return ControlEvent(events: source)
+    }
+
+    var didReceiveMemoryWarning: ControlEvent<Void> {
+        let source = self.methodInvoked(#selector(Base.didReceiveMemoryWarning)).map { _ in }
+        return ControlEvent(events: source)
+    }
+
+    var isVisible: Observable<Bool> {
+        let viewDidAppearObservable = self.base.rx.viewDidAppear.map { _ in true }
+        let viewWillDisappearObservable = self.base.rx.viewWillDisappear.map { _ in false }
+        return Observable<Bool>.merge(viewDidAppearObservable, viewWillDisappearObservable)
+    }
+
+    var isDismissing: ControlEvent<Bool> {
+        let source = self.sentMessage(#selector(Base.dismiss)).map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
+    }
+}
