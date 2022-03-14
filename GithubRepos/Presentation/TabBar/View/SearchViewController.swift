5ccdbc7e1236d72023fc27a2f94d8fd3d58ef5ec
Youngminah <bluebelsmudi@gmail.com>
2022-03-14 15:15:09 +0900

    feat: 서치 뷰컨
    페이지네이션, 리프래쉬, 로딩중, 초기값 등등

--- GithubRepos/Presentation/TabBar/View/SearchViewController.swift
+++ GithubRepos/Presentation/TabBar/View/SearchViewController.swift
@@ -12,11 +12,20 @@ import SnapKit
 
 final class SearchViewController: BaseViewController {
 
+    private let searchBar = SearchBar()
+    private let refreshControl = UIRefreshControl()
+    private let tableView = UITableView(frame: .zero, style: .insetGrouped)
+
     private lazy var input = SearchViewModel.Input(
+        pullRefresh: refreshControl.rx.controlEvent(.valueChanged).asSignal(),
+        didScrollToBottom: tableView.rx.didScrollToBottom.asSignal(onErrorJustReturn: ()),
+        searchBarText: searchBar.shouldLoadResult.asSignal(onErrorJustReturn: "")
     )
     private lazy var output = viewModel.transform(input: input)
     private var viewModel: SearchViewModel
     private let disposeBag = DisposeBag()
+
+    private let viewDidLoadEvent = PublishRelay<Void>()
 
     init(viewModel: SearchViewModel) {
         self.viewModel = viewModel
@@ -29,22 +38,83 @@ final class SearchViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        hideKeyboardWhenTappedAround()
         bind()
     }
 
     private func bind() {
+        output.repoList
+            .map { return $0.count <= 0 }
+            .drive(tableView.rx.isEmpty(
+                title: "검색 결과가 없어요.")
+            )
+            .disposed(by: disposeBag)
 
+        output.repoList
+            .drive(tableView.rx.items) { tv, index, element in
+                let cell = tv.dequeueReusableCell(withIdentifier: RepositoryCell.identifier) as! RepositoryCell
+                cell.configure(item: element)
+                return cell
+            }
+            .disposed(by: disposeBag)
+
+        output.refreshAction
+            .emit(to: tableView.rx.isRefresh())
+            .disposed(by: disposeBag)
+
+        output.bottomSpinnerAction
+            .emit(to: tableView.rx.isAnimatingBottomSpinner())
+            .disposed(by: disposeBag)
+
+        output.refreshAction
+            .emit(onNext: { [weak self] isRefresh in
+                if !isRefresh {
+                    self?.refreshControl.endRefreshing()
+                }
+            })
+            .disposed(by: disposeBag)
     }
 
     override func setViews() {
         super.setViews()
+        view.addSubview(searchBar)
+        view.addSubview(tableView)
     }
 
     override func setConstraints() {
         super.setConstraints()
+        searchBar.snp.makeConstraints { make in
+            make.top.left.right.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(50)
+        }
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom)
+            make.left.right.bottom.equalToSuperview()
+        }
     }
 
     override func setConfigurations() {
         super.setConfigurations()
+        searchBar.backgroundImage = UIImage()
+        searchBar.placeholder = "검색할 레파지토리를 입력해주세요."
+        tableView.register(RepositoryCell.self,
+                           forCellReuseIdentifier: RepositoryCell.identifier)
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.refreshControl = refreshControl
+        tableView.estimatedRowHeight = 80
+    }
+}
+
+// MARK: - 키보드 관련
+extension SearchViewController {
+
+    private func hideKeyboardWhenTappedAround() {
+        let tapGesture = UITapGestureRecognizer(target: self,
+                         action: #selector(hideKeyboard))
+        view.addGestureRecognizer(tapGesture)
+    }
+
+    @objc func hideKeyboard() {
+        view.endEditing(true)
     }
 }
