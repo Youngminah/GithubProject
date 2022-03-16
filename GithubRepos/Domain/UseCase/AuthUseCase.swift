cd7ab354f8ca557b1728469f973bf2b5a6ec7451
Youngminah <bluebelsmudi@gmail.com>
2022-03-17 04:34:20 +0900

    feat: auth 유즈케이스

--- GithubRepos/Domain/UseCase/AuthUseCase.swift
+++ GithubRepos/Domain/UseCase/AuthUseCase.swift
@@ -11,6 +11,38 @@ import RxSwift
 
 final class AuthUseCase {
 
+    private let authRepository: AuthRepositoryType
     private let disposeBag = DisposeBag()
 
+    let successLogin = PublishRelay<Void>()
+    let failGithubError = PublishRelay<GithubServerError>()
+
+    init(
+        authRepository: AuthRepositoryType
+    ){
+        self.authRepository = authRepository
+    }
+}
+
+extension AuthUseCase {
+
+    func requestLogin(code: String) {
+        let clientID = "23a655cd597603f9f109" // 임시
+        let clientSecret = "1dc45efc562ec73389d794310f17e1e5a74c0d50" // 임시
+        self.authRepository.requestToken(
+            clientID: clientID,
+            clientSecret: clientSecret,
+            code: code) { [weak self] response in
+                guard let self = self else { return }
+                switch response {
+                case .success(let info):
+                    print("로그인 성공!! -->", info)
+                    UserDefaults.standard.set(info.accessToken, forKey: "accessToken")
+                    self.successLogin.accept(())
+                case .failure(let error):
+                    print("로그인 실패!! -->", error)
+                    self.failGithubError.accept(error)
+                }
+            }
+    }
 }
